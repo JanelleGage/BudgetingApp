1a97b31ba35a13fb23046de2dd1c7b70709e4238
janellegage <jgagerob@asu.edu>
2023-09-08 12:38:59 -0400

    initial

---
+++ app/src/App.jsx
@@ -0,0 +1,42 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+//Routes
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+import Main, { mainLoader } from "./layouts/Main";
+import { logoutAction } from "./actions/logout";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />
+      },
+      {
+        path: "logout",
+        action: logoutAction
+      },
+    ]
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+export default App
